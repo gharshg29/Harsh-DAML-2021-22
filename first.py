5ce0919fcdca3460f10f2fa00de84ceca97189fb
gharshg29 <gharshgupta29@gmail.com>
2021-10-13 14:29:01 +0530

    Adding 2 Python Files

---
+++ first.py
@@ -0,0 +1,5 @@
+Python 3.9.4 (v3.9.4:1f2e3088f3, Apr  4 2021, 12:32:44) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def abc(a,b):
+	return a/b;
